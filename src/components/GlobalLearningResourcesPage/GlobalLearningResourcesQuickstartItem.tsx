ec40a69bfe0efe08c987bfbd019a1d0854a57494
Adam Jetmar <ajetmar@redhat.com>
2024-07-24 19:42:49 +0200

    Update quickstart cards

--- src/components/GlobalLearningResourcesPage/GlobalLearningResourcesQuickstartItem.tsx
+++ src/components/GlobalLearningResourcesPage/GlobalLearningResourcesQuickstartItem.tsx
@@ -11,6 +11,11 @@ import {
   GalleryItem,
   Icon,
   Label,
+  Stack,
+  StackItem,
+  Text,
+  TextContent,
+  TextVariants,
 } from '@patternfly/react-core';
 import { API_BASE, FAVORITES } from '../../hooks/useQuickStarts';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
@@ -19,13 +24,13 @@ import axios from 'axios';
 import './GlobalLearningResourcesQuickstartItem.scss';
 
 const OutlinedBookmarkedIcon = () => (
-  <Icon className="lr-c-bookmark__icon">
+  <Icon className="lr-c-global-learning-resources-quickstart__card--bookmark">
     <OutlinedBookmarkIcon />
   </Icon>
 );
 
 const BookmarkedIcon = () => (
-  <Icon className="lr-c-bookmark__icon">
+  <Icon className="lr-c-global-learning-resources-quickstart__card--bookmark-filled">
     <BookmarkIcon />
   </Icon>
 );
@@ -50,34 +55,45 @@ const GlobalLearningResourcesQuickstartItem: React.FC<
 
   return (
     <GalleryItem key={quickStart.metadata.name}>
-      <Card>
-        <CardTitle
-          component="div"
-          className="lr-c-global-learning-resources-quickstart__card--title"
-        >
-          <Flex>
-            <img
-              src="/apps/frontend-assets/console-landing/ansible.svg"
-              alt=""
-            />
-            <Flex direction={{ default: 'column' }}>
-              <Label color="orange">{quickStart.spec.type?.text}</Label>
-              <span className="lr-c-global-learning-resources-quickstart__card--title-span">
-                {quickStartURL.hostname}
-              </span>
+      <Card className="lr-c-global-learning-resources-quickstart__card">
+        <TextContent>
+          <CardTitle
+            component="div"
+            className="lr-c-global-learning-resources-quickstart__card--title"
+          >
+            <Flex>
+              <img
+                className="lr-c-global-learning-resources-quickstart__card--title-icon"
+                src="/apps/frontend-assets/console-landing/ansible.svg"
+                alt=""
+              />
+              <Stack hasGutter={false}>
+                <StackItem>
+                  <Label isCompact color="orange">
+                    {quickStart.spec.type?.text}
+                  </Label>
+                </StackItem>
+                <StackItem>
+                  <Text component={TextVariants.small}>
+                    {quickStartURL.hostname}
+                  </Text>
+                </StackItem>
+              </Stack>
             </Flex>
             <OutlinedBookmarkIcon />
-          </Flex>
-        </CardTitle>
-        <CardBody component="div">
-          <Flex direction={{ default: 'column' }}>
-            <span className="lr-c-global-learning-resources-quickstart__card--body-name">
+          </CardTitle>
+          <CardBody component="div">
+            <Text component={TextVariants.h3}>
               {quickStart.spec.displayName}
-            </span>
-            <p className="lr-c-global-learning-resources-quickstart__card--body-description">{quickStart.spec.description}</p>
-          </Flex>
-        </CardBody>
-        <CardFooter>Footer</CardFooter>
+            </Text>
+            <Text component={TextVariants.p}>
+              {quickStart.spec.description}
+            </Text>
+          </CardBody>
+          <CardFooter>
+            <Text component={TextVariants.small}>Footer</Text>
+          </CardFooter>
+        </TextContent>
       </Card>
       {/* <QuickStartTile
         action={{
